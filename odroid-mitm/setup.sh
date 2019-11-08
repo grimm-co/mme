85c0c7cd27fd1600e5de333f46046d5b5140f9f8
adam <adam@grimm-co.com>
2019-11-08 07:11:53 +0000

    Added a setup.sh script which will be the only thing one needs to run to get everything set up

---
+++ odroid-mitm/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This will run all the scripts to install the software, scripts that need to
+# be run on each boot and makes sure that every time the device boots, it is
+# routing/intercepting as much traffic as possible.
