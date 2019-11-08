7eda85959ae2ebc59e26bfb3d62704b7ff14c84c
adam <adam@grimm-co.com>
2019-11-08 05:45:44 +0000

    Tested install_dhcp on ubuntu

--- install_dhcp.sh
+++ install_dhcp.sh
@@ -2,6 +2,9 @@
 # This script will install a DHCP server, configure it, and run it on the
 # first en* network interface
 eth0=`ifconfig | grep ^en | head -n 1 | sed 's/:.*//g'`
+if [ -z "$eth0" ]; then
+  eth0="wlan1"  # serve up DHCP addresses on wlan1
+fi
 ip="192.168.101.1"
 network="192.168.101.0"
 netmask="255.255.255.0"
@@ -12,8 +15,11 @@ dhcp_end="192.168.101.100"
 
 distro_id=`grep ^ID= /etc/os-release | sed 's/.*=//g'`
 if [ "$distro_id" != "fedora" -a "$distro_id" != "debian" ]; then
-  echo "Unsupported distro: $distro_id"
-  exit 1
+  distro_id=`grep ^ID_LIKE= /etc/os-release | sed 's/.*=//g'`
+  if [ "$distro_id" != "fedora" -a "$distro_id" != "debian" ]; then
+    echo "Unsupported distro: $distro_id"
+    exit 1
+  fi
 fi
 
 # Set the host IP
@@ -27,14 +33,12 @@ if [ "$distro_id" = "fedora" ]; then
 else
   if [ "$distro_id" = "debian" ]; then
     echo "Untested on Debian-based distros!"
-    sudo apt install isc-dhcp-server       # Ubuntu
+    sudo apt-get install -y isc-dhcp-server # Ubuntu
   fi
-  #vi /etc/systemd/system/dhcpd.service
-  #sudo vim /etc/default/isc-dhcp-server  # Ubuntu
   service_name="isc-dhcp-server"
 fi
 
-echo "subnet $network netmask $netmask {"             | sudo tee -a /etc/dhcp/dhcpd.conf
+echo "subnet $network netmask $netmask {"             | sudo tee /etc/dhcp/dhcpd.conf
 echo "    option routers $ip;"                        | sudo tee -a /etc/dhcp/dhcpd.conf
 echo "    option subnet-mask $netmask;"               | sudo tee -a /etc/dhcp/dhcpd.conf
 echo "    option domain-search   \"$search_domain\";" | sudo tee -a /etc/dhcp/dhcpd.conf
@@ -42,7 +46,7 @@ echo "    option domain-name-servers $nameserver;"    | sudo tee -a /etc/dhcp/dh
 echo "    range $dhcp_start $dhcp_end;"               | sudo tee -a /etc/dhcp/dhcpd.conf
 echo "}"                                              | sudo tee -a /etc/dhcp/dhcpd.conf
 
-
+echo "Config file written"
 sudo systemctl start $service_name
 sudo systemctl status $service_name
 
