040caa499341b0ebbab9262f0c42433e3d1d505f
adam <adam@grimm-co.com>
2019-12-22 22:42:53 +0000

    Added some commands that are needed on some wifi cards

--- odroid-mitm/install_hostapd.sh
+++ odroid-mitm/install_hostapd.sh
@@ -8,6 +8,12 @@ sudo apt-get install -y hostapd
 echo "DAEMON_CONF=/etc/hostapd/hostapd.conf" | sudo tee -a /etc/default/hostapd
 sudo cp hostapd.conf /etc/hostapd/
 
+# hostapd won't be able to configure driver mode unless this is done:
+sudo nmcli radio wifi off
+sudo rfkill unblock wlan
+sudo ifconfig wlan0 up
+sudo ifconfig wlan1 up
+
 # Start hostapd
 sudo systemctl unmask hostapd
 
