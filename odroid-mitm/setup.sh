2bad01d817c4444dcc13a5c4bb96ded06582ec54
adam <adam@grimm-co.com>
2019-12-22 22:47:27 +0000

    Updated setup script to priv up if not run as root, also start masquerading immediately and install hostapd

--- odroid-mitm/setup.sh
+++ odroid-mitm/setup.sh
@@ -3,10 +3,19 @@
 # be run on each boot and makes sure that every time the device boots, it is
 # routing/intercepting as much traffic as possible.
 
+if [ `whoami` != root ]; then
+	sudo $0
+	exit $?
+fi
+
 # Install DHCP server
 ./install_dhcp.sh
 
+# Install hostapd so we can serve up an access point
+./install_hostapd.sh
+
 # Install scripts to do masqerading and IP forwarding
-sudo cp masquerade.sh /etc/rc.local
+./masquerade.sh   # start masquerading now
+sudo cp masquerade.sh /etc/rc.local  # and in the future
 ./enable_ip_forwarding.sh
 
